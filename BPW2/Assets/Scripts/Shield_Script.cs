44d4c38054945039380b3f9eceef3770287b7408
TBreemhaar <tingting.breemhaar@student.hku.nl>
2019-08-09 02:00:30 +0200

    audio toegevoegd

--- BPW2/Assets/Scripts/Shield_Script.cs
+++ BPW2/Assets/Scripts/Shield_Script.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Shield_Script : MonoBehaviour
 {
+    public AudioClip wub;
+    private AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         var enemy = collision.GetComponent<Enemy_Script>();
         if (enemy != null)
         {
+            source.PlayOneShot(wub);
             Destroy(enemy.gameObject);
             ScoreScript.scoreValue += 1;
         }
